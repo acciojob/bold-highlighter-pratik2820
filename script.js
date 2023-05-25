131dfd89becb638bd390e8d9b2ee46e71ebc93da
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-25 11:42:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+let elements = document.getElementByTagName("strong");
+
 function highlight() {
     //Write your code here
-
+	for(let i=0;i<elements.length;i++){
+		elements[i].style.color = "green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	for(let i=0;i<elements.length;i++){
+		elements[i].style.color = "black";
+	}
     
 }
